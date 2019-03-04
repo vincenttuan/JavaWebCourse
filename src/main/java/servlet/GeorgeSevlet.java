171fa45a4aa719eed7d3d5442c86aa2468b7021e
student <student@192.168.2.19>
2019-03-04 20:41:14 +0800

    JavaWeb

---
+++ src/main/java/servlet/GeorgeSevlet.java
@@ -0,0 +1,17 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class GeorgeSevlet extends GenericServlet{
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        
+    }
+
+    
+}
