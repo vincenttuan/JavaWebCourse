f669f10eaaa2456474e77f82ffd743b2da12bbf3
student <student@192.168.2.19>
2019-03-04 21:12:55 +0800

    JavaWeb

--- src/main/java/servlet/GeorgeSevlet.java
+++ src/main/java/servlet/GeorgeSevlet.java
@@ -2,6 +2,9 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
 import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -12,7 +15,12 @@ public class GeorgeSevlet extends GenericServlet{
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
         PrintWriter out = res.getWriter();
-        out.print("Hello GeorgeSevlet 123");
+        Set<Integer> lotto = new TreeSet<>();
+        while(lotto.size() < 6) {
+            int number = new Random().nextInt(46) + 1;
+            lotto.add(number);
+        }
+        out.print("Lotto number = " + lotto);
     }
 
     
