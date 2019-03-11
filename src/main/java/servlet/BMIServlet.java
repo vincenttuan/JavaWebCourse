b874620a27b08411c080d6482d061c12aafa9505
student <student@192.168.2.19>
2019-03-11 21:36:54 +0800

    JavaWeb

--- src/main/java/servlet/BMIServlet.java
+++ src/main/java/servlet/BMIServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 public class BMIServlet extends HttpServlet{
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String height = req.getParameter("height");
         String weight = req.getParameter("weight");
         double h = Double.parseDouble(height);
