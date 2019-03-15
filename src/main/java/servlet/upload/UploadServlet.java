692e1796dc87afcfbd298a34ffa5d04622645af4
student <student@192.168.2.19>
2019-03-15 19:40:27 +0800

    JavaWeb

---
+++ src/main/java/servlet/upload/UploadServlet.java
@@ -0,0 +1,49 @@
+package servlet.upload;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import utils.Util;
+
+@WebServlet("/servlet/upload/UploadServlet")
+@MultipartConfig(
+                 fileSizeThreshold=1024*1024*2, // 2MB
+                 maxFileSize=1024*1024*10,      // 10MB
+                 maxRequestSize=1024*1024*50)   // 50MB
+public class UploadServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/plain;chartset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        
+        String title = "";
+        for (Part part : req.getParts()) {
+            switch (part.getName()) {
+                case "title":
+//                    InputStream is = part.getInputStream();
+//                    byte[] b = new byte[1];
+//                    while(is.read(b) != -1) {
+//                        out.print((char)b[0]);
+//                    }
+                    title = Util.getValue(part.getInputStream());
+                    break;
+                case "upload":
+                    out.println("get upload");
+                    break;
+            }
+        }
+        
+        out.append(title);
+        out.print("Upload OK");
+    }
+    
+}
---
+++ src/main/java/utils/Util.java
@@ -0,0 +1,121 @@
+package utils;
+
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Base64;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.Part;
+
+public class Util {
+
+    public static String getFileName(Part part) {
+        String partHeader = part.getHeader("content-disposition");
+        for (String content : part.getHeader("content-disposition").split(";")) {
+            if (content.trim().startsWith("filename")) {
+                return content.substring(
+                        content.indexOf('=') + 1).trim().replace("\"", "");
+            }
+        }
+        return null;
+    }
+
+    public static boolean saveFile(InputStream inputStream, String filePathAndName) {
+        try {
+            byte[] buffer = new byte[inputStream.available()];
+            inputStream.read(buffer);
+            File targetFile = new File(filePathAndName);
+            FileOutputStream outStream = new FileOutputStream(targetFile);
+            outStream.write(buffer);
+            outStream.flush();
+            outStream.close();
+            return targetFile.exists();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    public static boolean saveFile(String base64, String filePathAndName) {
+        try {
+            byte[] buffer = getDeBase64(base64);
+            File targetFile = new File(filePathAndName);
+            FileOutputStream outStream = new FileOutputStream(targetFile);
+            outStream.write(buffer);
+            outStream.flush();
+            outStream.close();
+            return targetFile.exists();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    public static String getValue(InputStream is) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+        BufferedReader reader = new BufferedReader(isr);
+        String data = "";
+        while ((data = reader.readLine()) != null) {
+            sb.append(data);
+        }
+        return sb.toString();
+    }
+
+    public static String getBase64(String filepath) throws IOException {
+        InputStream is = new FileInputStream(filepath);
+        return getBase64(is);
+    }
+
+    public static String getBase64(InputStream is) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] bytes = new byte[1];
+        while (is.read(bytes) != -1) {
+            baos.write(bytes);
+        }
+        String base64 = Base64.getEncoder().encodeToString(baos.toByteArray());
+        return base64;
+    }
+
+    public static byte[] getDeBase64(String base64) throws IOException {
+        return Base64.getDecoder().decode(base64);
+    }
+
+    public static String getBase64(InputStream is, int scaledWidth, int scaledHeight) throws IOException {
+        Image image = ImageIO.read(is);
+        BufferedImage bi = createResizedCopy(image, scaledWidth, scaledHeight, true);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(bi, "jpg", baos);
+        String base64 = Base64.getEncoder().encodeToString(baos.toByteArray());
+        return base64;
+    }
+
+    private static BufferedImage createResizedCopy(Image originalImage, int scaledWidth, int scaledHeight, boolean preserveAlpha) {
+        int imageType = preserveAlpha ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+        BufferedImage scaledBI = new BufferedImage(scaledWidth, scaledHeight, imageType);
+        Graphics2D g = scaledBI.createGraphics();
+        if (preserveAlpha) {
+            g.setComposite(AlphaComposite.Src);
+        }
+        g.drawImage(originalImage, 0, 0, scaledWidth, scaledHeight, null);
+        g.dispose();
+        return scaledBI;
+    }
+
+}
