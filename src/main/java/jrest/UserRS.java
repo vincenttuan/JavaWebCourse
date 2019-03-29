11cb7be17417ddd9300bfe10a151fb5ef92f37bf
student <student@192.168.2.19>
2019-03-29 21:10:35 +0800

    JavaWeb

--- src/main/java/jrest/UserRS.java
+++ src/main/java/jrest/UserRS.java
@@ -2,6 +2,7 @@ package jrest;
 
 import com.google.gson.Gson;
 import dao.UserDAO;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -24,6 +25,13 @@ public class UserRS {
     @Path("/all")
     public String queryAll() {
         return new Gson().toJson(dao.queryAll());
+    }
+    
+    @DELETE
+    @Path("/{id}")
+    public String delete(@PathParam("id") int id) {
+        dao.delete(id);
+        return "Delete OK";
     }
     
     
