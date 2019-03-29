b158c8abac80ea956d6906a698c1a6d4772f36b9
student <student@192.168.2.19>
2019-03-29 21:01:56 +0800

    JavaWeb

---
+++ src/main/java/jrest/UserRS.java
@@ -0,0 +1,30 @@
+package jrest;
+
+import com.google.gson.Gson;
+import dao.UserDAO;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+@Path("/user")
+public class UserRS {
+    private UserDAO dao;
+    
+    {
+        dao = new UserDAO();
+    }
+    
+    @GET
+    @Path("/{id}")
+    public String get(@PathParam("id") int id) {
+        return new Gson().toJson(dao.get(id));
+    }
+    
+    @GET
+    @Path("/all")
+    public String queryAll() {
+        return new Gson().toJson(dao.queryAll());
+    }
+    
+    
+}
