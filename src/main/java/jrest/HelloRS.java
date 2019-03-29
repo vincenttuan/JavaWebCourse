02e130abc56a4deb918f0f14ca0c8d99ab47a313
student <student@192.168.2.19>
2019-03-29 19:16:10 +0800

    JavaWeb

--- src/main/java/jrest/HelloRS.java
+++ src/main/java/jrest/HelloRS.java
@@ -1,13 +1,29 @@
 package jrest;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 
 @Path("/hello")
 public class HelloRS {
+    
     @GET
-    public String sayHelloWorld() {
-        return "Hello world";
+    public String sayHello() {
+        return "Hello !";
     }
+    
+    @GET
+    @Path("/lotto")
+    public String getLotto() {
+        Set<Integer> set = new HashSet<>();
+        while(set.size() < 6) {
+            set.add(new Random().nextInt(46) + 1);
+        }
+        return set.toString();
+    }
+    
+    
 }
