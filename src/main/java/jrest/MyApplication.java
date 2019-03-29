7f977dcbdad34900ef84d57e2da495b53c3565dd
student <student@192.168.2.19>
2019-03-29 18:58:52 +0800

    JavaWeb

---
+++ src/main/java/jrest/MyApplication.java
@@ -0,0 +1,12 @@
+package jrest;
+
+import javax.ws.rs.ApplicationPath;
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("jrest")
+public class MyApplication extends ResourceConfig{
+    public MyApplication(){
+        
+    }
+}
+
