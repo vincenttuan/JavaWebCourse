b69894a5363591aad6b57fc5e03d66f4c91ff7fe
student <student@192.168.2.19>
2019-04-12 19:53:27 +0800

    JavaWeb

---
+++ src/main/java/myjpa/JPA_UserServlet.java
@@ -0,0 +1,28 @@
+package myjpa;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/myjpa/JPA_UserServlet")
+public class JPA_UserServlet extends HttpServlet {
+    private JPA_UserController controller = new JPA_UserController();
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Gson gson = new Gson();
+        PrintWriter out = resp.getWriter();
+        out.print(gson.toJson(controller.queryAll()));
+    }
+    
+    
+
+}
