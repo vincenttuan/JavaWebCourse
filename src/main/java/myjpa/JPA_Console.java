43d2be371904cffabe14bf4c5a0dcb521909e8a1
student <student@192.168.2.19>
2019-04-12 19:39:03 +0800

    JavaWeb

--- src/main/java/myjpa/JPA_Console.java
+++ src/main/java/myjpa/JPA_Console.java
@@ -6,28 +6,24 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Query;
 
 public class JPA_Console {
-    
+
     private static EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-        
+
     public static void main(String[] args) {
 
 //        create(new User("Vincent", 30));
 //        create(new User("Anita", 20));
 //        create(new User("John", 10));
-
 //        System.out.println(getUser(1L));
-
 //        System.out.println(queryUsers());
-        
 //        System.out.println(query());
-
-        update();
-
+//        update();
+        delete();
         JPAUtil.shutdown();
     }
 
     public static void create(User user) {
-        
+
         // 進入交易模式
         EntityTransaction etx = em.getTransaction();
         // 交易開始
@@ -36,11 +32,11 @@ public class JPA_Console {
         em.persist(user);
         // 交易確認
         etx.commit();
-        
+
         System.out.println("1.新增完畢成功");
         //em.close();
     }
-    
+
     public static User getUser(long id) {
         EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
         User user = em.find(User.class, id);
@@ -55,7 +51,7 @@ public class JPA_Console {
         em.close();
         return list;
     }
-    
+
     public static List query() {
         EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
         Query query = em.createNativeQuery("Select id, name, age from APP.T_USER", User.class);
@@ -63,12 +59,12 @@ public class JPA_Console {
         em.close();
         return list;
     }
-    
+
     public static void update() {
         EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
         User user = em.find(User.class, 4L);
         System.out.println(user);
-        if(user != null) {
+        if (user != null) {
             user.setName("Tom");
             user.setAge(10);
             EntityTransaction etx = em.getTransaction();
@@ -77,6 +73,21 @@ public class JPA_Console {
             etx.commit();
             System.out.println("2.修改成功");
             System.out.println(user);
+        }
+        em.close();
+
+    }
+
+    public static void delete() {
+        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+        User user = em.find(User.class, 6L);
+        System.out.println(user);
+        if (user != null) {
+            EntityTransaction etx = em.getTransaction();
+            etx.begin();
+            em.remove(user); // em.remove(em.merge(user)); 看Entity 的狀態為何
+            etx.commit();
+            System.out.println("3.刪除成功");
         }
         em.close();
 
