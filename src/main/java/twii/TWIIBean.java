a3e29a7e8d6e85c0b34c276f38f28251c3d65a16
student <student@192.168.2.19>
2019-04-15 19:27:29 +0800

    JavaWeb

--- src/main/java/twii/TWIIBean.java
+++ src/main/java/twii/TWIIBean.java
@@ -1,6 +1,7 @@
 package twii;
 
 import java.io.IOException;
+import java.util.stream.Stream;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -22,7 +23,13 @@ public class TWIIBean {
     public static void main(String[] args) throws IOException {
         String url = "http://www.twse.com.tw/exchangeReport/BWIBBU_d?response=csv&date=20190415&selectType=ALL";
         TWIIBean twiib = new TWIIBean();
-        String data = twiib.run(url);
-        System.out.println(data);
+        String[] data = twiib.run(url).split("\n");
+        System.out.println(data.length);
+        Stream.of(data)
+                .map(n -> n.replace("\"", ""))
+                .map(n -> n.replace("-", "-1"))
+                .map(n -> n.split(","))
+                .filter(n -> n.length == 8 && !n[0].equals("證券代號"))
+                .forEach(n -> System.out.println(n[0] + " " + n[1] + " " + n[2] + " " + n[3] + " " + n[4] + " " + n[5] + " " + n[7]));
     }
 }
