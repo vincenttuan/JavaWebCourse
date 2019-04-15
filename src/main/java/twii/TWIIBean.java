02cd459cd4e5f7102264e624c1fc14311f68286a
student <student@192.168.2.19>
2019-04-15 19:41:26 +0800

    JavaWeb

--- src/main/java/twii/TWIIBean.java
+++ src/main/java/twii/TWIIBean.java
@@ -1,6 +1,9 @@
 package twii;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -25,11 +28,18 @@ public class TWIIBean {
         TWIIBean twiib = new TWIIBean();
         String[] data = twiib.run(url).split("\n");
         System.out.println(data.length);
+        double yield = 7;
+        double pe = 5;
+        double pb = 1;
         Stream.of(data)
                 .map(n -> n.replace("\"", ""))
                 .map(n -> n.replace("-", "-1"))
                 .map(n -> n.split(","))
                 .filter(n -> n.length == 8 && !n[0].equals("證券代號"))
+                .filter(n -> Double.parseDouble(n[2]) >= yield)
+                .filter(n -> Double.parseDouble(n[4]) < pe)
+                .filter(n -> Double.parseDouble(n[5]) < pb)
                 .forEach(n -> System.out.println(n[0] + " " + n[1] + " " + n[2] + " " + n[3] + " " + n[4] + " " + n[5] + " " + n[7]));
+        
     }
 }
