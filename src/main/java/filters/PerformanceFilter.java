04e6c9fd103f824e9fe3a2988c41ba955b5d8800
student <student@192.168.2.19>
2019-04-19 21:43:24 +0800

    JavaWeb

--- src/main/java/filters/PerformanceFilter.java
+++ src/main/java/filters/PerformanceFilter.java
@@ -19,6 +19,7 @@ public class PerformanceFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("PerformanceFilter");
         long begin = System.currentTimeMillis();
         chain.doFilter(request, response);
         long end = System.currentTimeMillis();
