47013ad845e18e96624937d26bcbe8075f057952
student <student@192.168.2.19>
2019-04-22 19:53:56 +0800

    JavaWeb

---
+++ src/main/java/servlet/SessionServlet.java
@@ -0,0 +1,33 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/servlet/SessionServlet")
+public class SessionServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        resp.getWriter().print("create Session");
+        if(session.isNew()) {
+            System.out.println("New:" + session.getId());
+        }
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if(session != null) {
+            System.out.println("Del:" + session.getId());
+            session.invalidate();
+            resp.getWriter().print("destroy Session");
+        }
+    }
+    
+}
