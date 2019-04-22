68ea1ed83e2987097ff5cf39d98e21311e1a03e0
student <student@192.168.2.19>
2019-04-22 20:03:59 +0800

    JavaWeb

---
+++ src/main/java/servlet/SessionServletStatus.java
@@ -0,0 +1,24 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/servlet/SessionServletStatus")
+public class SessionServletStatus extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if(session == null) {
+            resp.getWriter().print("status = logout");
+        } else {
+            resp.getWriter().print("status = login");
+        }
+    }
+    
+}
